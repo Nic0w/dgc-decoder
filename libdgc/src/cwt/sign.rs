8d84d343f247278b194653a5cf17bc4bb17b0185
Nic0w <nico.broquet@gmail.com>
2022-01-19 23:10:13 +0100

    Zero copy signature !

--- libdgc/src/cwt/sign.rs
+++ libdgc/src/cwt/sign.rs
@@ -1,17 +1,17 @@
-use serde_bytes::ByteBuf;
+use serde_bytes::{ByteBuf, Bytes};
 
 use serde::ser::{Serialize, Serializer};
 
 const SIGN1_CONTEXT_STRING: &str = "Signature1";
 
-struct Sig {
-    context: String,
-    body_protected: ByteBuf,
-    external_aad: ByteBuf,
-    payload: ByteBuf,
+struct Sig<'a> {
+    context: &'static str,
+    body_protected: &'a Bytes,
+    external_aad:  &'a Bytes,
+    payload:  &'a Bytes,
 }
 
-impl Serialize for Sig {
+impl<'a> Serialize for Sig<'a> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
@@ -28,11 +28,12 @@ impl Serialize for Sig {
 
 pub fn get_validation_data(protected: &[u8], content: &[u8]) -> Vec<u8> {
     serde_cbor::ser::to_vec_packed(&Sig {
-        context: SIGN1_CONTEXT_STRING.into(),
+        context: SIGN1_CONTEXT_STRING,
 
-        body_protected: ByteBuf::from(protected),
-        external_aad: ByteBuf::with_capacity(0),
-        payload: ByteBuf::from(content),
+        body_protected: Bytes::new(protected),
+        external_aad: Bytes::new(b""),
+        payload: Bytes::new(content),
+        
     })
     .expect("Failed to get Sig structure as bytes.")
 }
