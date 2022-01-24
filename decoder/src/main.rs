597978962753369cad8d4a587e05a8764ee7aa84
Nic0w <nico.broquet@gmail.com>
2022-01-24 23:14:36 +0100

    Newer clap !

--- decoder/src/main.rs
+++ decoder/src/main.rs
@@ -1,46 +1,50 @@
-use std::str::FromStr;
+use std::{str::FromStr, path::{Path, PathBuf}};
 
 use libkeystore::KeyStore;
+use log::LevelFilter;
 use reqwest::Url;
 
-use clap::{App, Arg, SubCommand};
+use clap::{App, Arg, Parser};
 
 const FR_DSC_URL: &'static str =
     "https://app.tousanticovid.gouv.fr/json/version-33/Certs/dcc-certs.json";
 
+/// Digital Green Certificate decoder
+#[derive(Parser, Debug)]
+#[clap(about, version, author = "Nic0w")]
+struct CommandLineInterface {
+
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: usize,
+    
+    #[clap(short, long)]
+    keystore: Option<String>,
+
+    image: Option<PathBuf>,
+}
+
+
 fn main() {
 
-    let _ = setup_logger();
+    let args = CommandLineInterface::parse();
 
-    let args = App::new("Digital Green Certificate decoder")
-        .version("1.0")
-        .author("nic0w")
-        .about("Does things!")
-        .arg(
-            Arg::new("images")
-                .value_name("FILES")
-                .help("Paths of images to scan")
-                .required(true)
-                .min_values(1)
-                .multiple(true),
-        )
-        .arg(
-            Arg::new("keystore")
-                .long("keystore")
-                .short('k')
-                .takes_value(true)
-                .value_name("PATH or URL")
-                .help("Location of a keystore. This enables signature verification of the DGCs."),
-        )
-        .get_matches();
+    let log_level = match args.verbose {
 
-    let keystore = args
-        .value_of("keystore")
-        .map(|text| {
-            if let Ok(url) = Url::from_str(text) {
+        0 => LevelFilter::Warn,
+        1 => LevelFilter::Info,
+        2 => LevelFilter::Debug,
+        
+        _ => LevelFilter::Trace
+    };
+
+    let _ = setup_logger(log_level);
+
+    let keystore = args.keystore
+        .map(|s| {
+            if let Ok(url) = Url::from_str(&s) {
                 libkeystore::load_from_url(url)
             } else {
-                libkeystore::load_from_file(text)
+                libkeystore::load_from_file(&s)
             }
         })
         .transpose()
@@ -55,19 +59,16 @@ fn main() {
 
                 e => panic!("Unreachable : {:?}", e),
             }
-        })
-        .unwrap();
+        }).unwrap();
 
-    if let Some(images) = args.values_of("images") {
-        for image in images {
-            scan_image(image, keystore.as_ref());
-        }
+    if let Some(image) = args.image {
+        scan_image(&image, keystore.as_ref());
     }
 }
 
-fn scan_image(image: &str, keystore: Option<&KeyStore>) {
+fn scan_image<P: AsRef<Path>>(image: P, keystore: Option<&KeyStore>) {
 
-    log::info!("Searching certificates in image: {}", image);
+    log::info!("Searching certificates in image: {}", image.as_ref().to_string_lossy());
 
     match libdgc::decode_image(image) {
         Ok(scanned) => {
@@ -79,7 +80,9 @@ fn scan_image(image: &str, keystore: Option<&KeyStore>) {
                 match (raw_cert.decode(), keystore) {
                     (Ok(decoded), Some(keystore)) => match decoded.verify_signature(keystore) {
                         Ok(verified) => {
-                            log::info!("Signature is valid.\n{}", verified)
+                            log::info!("Signature is valid.");
+
+                            println!("{}", verified);
                         }
                         Err(_e) => {
                             log::error!("Bad signature !")
@@ -102,14 +105,12 @@ fn scan_image(image: &str, keystore: Option<&KeyStore>) {
     }
 }
 
-fn setup_logger() -> Result<(), fern::InitError> {
-
-    use log::LevelFilter::*;
+fn setup_logger(level: LevelFilter) -> Result<(), fern::InitError> {
 
     fern::Dispatch::new()
-        .level(Debug)
-        //.level_for("libkeystore", Trace)
-        //.level_for("dgc", Trace)
+        .level(level)
+        .level_for("keystore", level)
+        .level_for("dgc", level)
         .format(|out, message, record| {
             out.finish(format_args!(
                 "[{}][{}] {}",
